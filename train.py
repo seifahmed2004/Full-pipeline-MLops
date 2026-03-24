958813c59b327d269c0393074878bc336228a72f
Seif Ahmed <seifahmedafifi@gmail.com>
2026-03-24 17:22:40 +0200

    Improve model accuracy

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 with mlflow.start_run() as run:
-    model = RandomForestClassifier(n_estimators=50, random_state=42)
+    model = RandomForestClassifier(n_estimators=200, max_depth=10, random_state=42)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
